3ccc9a737633294c732ac9fde60fcc245d60e43e
Chen Ben <chenben@zhuojianchina.com>
2019-01-15 13:07:46 +0800

    跨域、https配置

---
+++ yixinger-server/src/main/java/com/cb/yixinger/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.cb.yixinger.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @Description: 配置跨域
+ * @author: YFZX-CB-1784 ChenBen
+ * @create: 2019-01-15 10:02
+ **/
+@Configuration
+public class CorsConfig {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // 设置访问源地址
+        corsConfiguration.addAllowedOrigin("*");
+        // 设置访问源请求头
+        corsConfiguration.addAllowedHeader("*");
+        // 设置访问源请求方法
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // 对接口配置跨域设置
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
